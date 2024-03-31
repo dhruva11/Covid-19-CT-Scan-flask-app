35b5244d005c650a386a9f3b4f177741476ada43
Dhruv <dhruv.manoj9@gmail.com>
2024-03-31 09:51:10 +0530

    initial

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,5 +1,4 @@
-# wsgi.py
-from main import covid_ct  # Adjust 'main' to your Python file name and 'covid_ct' to your Flask app instance
+from main import covid_ct
 
 if __name__ == "__main__":
     covid_ct.run()
